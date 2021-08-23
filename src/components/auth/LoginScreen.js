9ee0fcafae4ad2508361c5e03892c5a9894f165b
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-08-23 15:18:28 -0500

    Login styles

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const LoginScreen = () => {
+  return (
+    <>
+      <h3 className="auth__title mb-5">Login</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="email"
+          autocomplete="off"
+          name="email"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Login
+        </button>
+        <div className="auth__social-network">
+          <p>Login with social networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to="/register" className="link">
+          Create new account
+        </Link>
+      </form>
+    </>
+  );
+};
