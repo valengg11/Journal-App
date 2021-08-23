57cb0953630bed3243e3de5de760ae3f0424567e
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-08-23 16:24:05 -0500

    Sidebar

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const JournalEntry = () => {
+  return (
+    <div className="journal__entry">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://media-exp1.licdn.com/dms/image/C4E1BAQFhqtOBNMQ5EA/company-background_10000/0/1519802595418?e=1629842400&v=beta&t=_b8UNU7cKtM7u1jmMQPzyjO4BTEkEVD4XC09JqX3AyU)",
+        }}
+      ></div>
+    </div>
+  );
+};
