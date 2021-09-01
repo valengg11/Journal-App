b9a66246f1aaa29de68d889d7b012005056db79e
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-09-01 14:35:40 -0500

    Journal screen date

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,10 +1,18 @@
 import React from "react";
+import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 import { startSaveNote, startUploading } from "../../redux/actions/notes";
 
 export const NotesAppBar = () => {
   const dispatch = useDispatch();
+
   const { active } = useSelector((state) => state.notes);
+  
+  const { date } = active;
+
+  const screenDate = moment(date)
+
+  console.log(date);
   const handleSave = () => {
     dispatch(startSaveNote(active));
   };
@@ -12,17 +20,17 @@ export const NotesAppBar = () => {
     document.querySelector("#fileSelector").click();
   };
   const handleFileChange = (e) => {
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     if (file) {
-      dispatch(startUploading(file))
+      dispatch(startUploading(file));
     }
   };
   return (
     <div className="notes__appbar">
-      <span>11 de agosto</span>
+      <span>{screenDate.format("dddd, MMMM Do YYYY")}</span>
       <input
         id="fileSelector"
-        name = "file"
+        name="file"
         type="file"
         style={{ display: "none" }}
         onChange={handleFileChange}
