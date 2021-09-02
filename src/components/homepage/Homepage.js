652d6468984aaf71afddf5113ede3947a1321efc
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-09-02 10:14:50 -0500

    Home page

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Homepage = () => {
+  return (
+    <div className="homepage__background">
+        <div className="homepage__buttons">
+        <Link to="/auth/register" className="btn btn-primary">
+          Register
+        </Link>
+        <Link to="/auth/login" className="btn btn-primary">
+          Login
+        </Link>
+        </div>
+        <img
+          className="homepage__image"
+          src="/images/journal_app.png"
+          alt="logo_journal_app"
+        />
+    </div>
+  );
+};
